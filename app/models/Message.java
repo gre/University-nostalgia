b9690d64b8187fd9aa6b4d5e08705e0533a6b50a
Renaudeau Gaetan <gre@zenexity.fr>
2010-03-16 19:44:03 +0100

    Improve message (deletable).

--- app/models/Message.java
+++ app/models/Message.java
@@ -20,11 +20,16 @@ public class Message extends Model {
     
     public Date date;
     
+    public boolean deleteBySender;
+    public boolean deleteByReceiver;
+    
     public Message(User sender, User receiver, String content) {
         this.sender = sender;
         this.receiver = receiver;
         this.content = content;
         this.date = new Date();
+        deleteBySender = false;
+        deleteByReceiver = false;
     }
     
     public boolean userConcerned(Long userid) {
@@ -32,9 +37,9 @@ public class Message extends Model {
     }
     
     public static List<Message> userSendbox(Long userid) {
-        return find("sender.id = ?1", userid).fetch();
+        return find("sender.id = ?1 and deleteBySender = FALSE", userid).fetch();
     }
     public static List<Message> userInbox(Long userid) {
-        return find("receiver.id = ?1", userid).fetch();
+        return find("receiver.id = ?1 and deleteByReceiver = FALSE", userid).fetch();
     }
 }
